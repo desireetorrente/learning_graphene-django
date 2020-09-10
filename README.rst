9ae2bb46a5a218b8527cb561e37c4114893f6c54
Desi <dt@botxo.co>
2020-09-10 15:21:59 +0200

    tutorial relay

--- README.rst
+++ README.rst
@@ -5,6 +5,11 @@ Proyecto para aprender Graphene-Django e integrar en un proyecto Django 3.1
 
 Documenatción: https://docs.graphene-python.org/projects/django/en/latest/
 
+DjangoObjectType class
+^^^^^^^^^^^^^^^^^^^^^^^^
+Define los campos de nuestros modelos de Django directamente en campo de GraphQL
+
+
 .. note::
     Este proyecto tiene el layout por defecto de Django pero no es
     el deseable.
---
+++ cookbook/ingredients/schema.py
@@ -0,0 +1,34 @@
+from graphene import relay, ObjectType
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+
+from cookbook.ingredients.models import Category, Ingredient
+
+
+# Graphene mapea el id del modelo con el Nodo
+class CategoryNode(DjangoObjectType):
+    class Meta:
+        model = Category
+        filter_fields = ['name', 'ingredients']
+        interfaces = (relay.Node, )
+
+
+class IngredientNode(DjangoObjectType):
+    class Meta:
+        model = Ingredient
+        # Allow for some more advanced filtering here
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'notes': ['exact', 'icontains'],
+            'category': ['exact'],
+            'category__name': ['exact'],
+        }
+        interfaces = (relay.Node, )
+
+
+class Query(ObjectType):
+    category = relay.Node.Field(CategoryNode)
+    all_categories = DjangoFilterConnectionField(CategoryNode)
+
+    ingredient = relay.Node.Field(IngredientNode)
+    all_ingredients = DjangoFilterConnectionField(IngredientNode)
